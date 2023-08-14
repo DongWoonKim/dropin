c94559c0e04d34f0ceb9283b9d1ece7423795232
DongWoonKim <01030985843z@naver.com>
2023-08-14 14:44:32 +0900

    prj build up

---
+++ src/test/java/com/example/dropin/DropinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dropin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
